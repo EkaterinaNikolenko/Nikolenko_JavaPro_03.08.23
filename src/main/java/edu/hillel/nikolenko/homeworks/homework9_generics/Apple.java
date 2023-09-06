94260bd653805df6c0a6bf4eb81dd065c99bf812
Ekaterina Nikolenko <nikolenkoekaterina@gmail.com>
2023-09-06 23:09:51 +0300

    hw9

---
+++ src/main/java/edu/hillel/nikolenko/homeworks/homework9_generics/Apple.java
@@ -0,0 +1,8 @@
+package edu.hillel.nikolenko.homeworks.homework9_generics;
+
+public class Apple implements Fruit {
+    private float weight = 1.0F;
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ src/main/java/edu/hillel/nikolenko/homeworks/homework9_generics/Box.java
@@ -0,0 +1,42 @@
+package edu.hillel.nikolenko.homeworks.homework9_generics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Box<T extends Fruit> {
+    private List<T> box = new ArrayList<>();
+    int amountOfFruits = 0;
+    public void addOneFruit(T t) {
+        box.add(t);
+    }
+    public void addFruits(List<T> fruits) {
+        for (T t: fruits) {
+            box.add(t);
+        }
+    }
+
+    public int getAmountOfFruits() {
+        return box.size();
+    }
+
+
+    public List<T> getBox() {
+        return box;
+    }
+
+    public float getWeight() {
+//        System.out.println();
+        return (float) box.size() * box.get(0).getWeight();
+    }
+    public boolean compare(Box<? extends Fruit> box) {
+        if(getWeight() == box.getWeight()) return true;
+        return false;
+    }
+    public void merge(Box<T> anotherBox) {
+        for (T t : anotherBox.getBox()) {
+            box.add(t);
+        }
+        anotherBox.getBox().clear();
+    }
+}
