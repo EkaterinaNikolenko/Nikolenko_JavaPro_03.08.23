d12e7ba80c9716d49fcb6f120b6a146d48c3be25
Ekaterina Nikolenko <nikolenkoekaterina@gmail.com>
2023-10-10 20:58:42 +0300

    hw15

---
+++ src/main/java/edu/hillel/nikolenko/homeworks/homework15_multithreading/Main.java
@@ -0,0 +1,8 @@
+package edu.hillel.nikolenko.homeworks.homework15_multithreading;
+
+public class Main {
+    public static void main(String[] args) {
+        ValueCalculator valueCalculator = new ValueCalculator(1000000);
+        valueCalculator.method();
+    }
+}
